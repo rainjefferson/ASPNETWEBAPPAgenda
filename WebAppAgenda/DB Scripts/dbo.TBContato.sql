077d0c7be3823d530c0b6adcb1cd3083b2abf79b
rainjefferson <117676799+rainjefferson@users.noreply.github.com>
2023-10-12 23:24:19 -0300

    Primeiro commit projeto Agenda online

---
+++ WebAppAgenda/DB Scripts/dbo.TBContato.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[tbcontato]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [nome] NVARCHAR(50) NOT NULL, 
+    [email] NVARCHAR(50) NOT NULL, 
+    [telefone] NVARCHAR(20) NOT NULL
+)
