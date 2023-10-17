c3779469cefc9f0ff44d8808218f1014c2865555
rainjefferson <117676799+rainjefferson@users.noreply.github.com>
2023-10-16 23:04:02 -0300

    Implementado pagina de login

---
+++ WebAppAgenda/WebAppAgenda/login.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="login.aspx.cs" Inherits="WebAppAgenda.login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server" Text="E-mail"></asp:Label>
+            <br />
+            <asp:TextBox ID="txtEmail" runat="server" Width="288px"></asp:TextBox>
+            <br />
+            <asp:Label ID="Label2" runat="server" Text="Senha"></asp:Label>
+            <br />
+            <asp:TextBox ID="txtSenha" runat="server" TextMode="Password" Width="174px"></asp:TextBox>
+            <br />
+            <asp:Button ID="btnLogin" runat="server" OnClick="btnLogin_Click" Text="Login" Width="153px" />
+            <br />
+            <br />
+            <asp:Label ID="lMsg" runat="server" BorderColor="Red" BorderStyle="Solid" Font-Bold="True"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebAppAgenda/WebAppAgenda/login.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebAppAgenda
+{
+    public partial class login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            String email = txtEmail.Text;
+            String senha = txtSenha.Text;
+
+            //capturar a string de conexão
+            System.Configuration.Configuration rootWebConfig = System.Web.Configuration.WebConfigurationManager.OpenWebConfiguration("/MyWebSiteRoot");
+            System.Configuration.ConnectionStringSettings connString;
+            connString = rootWebConfig.ConnectionStrings.ConnectionStrings["ConnectionString"];
+
+            //cria um objeto de conexão
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = connString.ToString();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            cmd.CommandText = "select * from tbusuario where email = @email and senha = @senha";
+            cmd.Parameters.AddWithValue("email", email);
+            cmd.Parameters.AddWithValue("senha", senha);
+            con.Open();
+            SqlDataReader registro = cmd.ExecuteReader();
+            if (registro.HasRows)
+            {
+                //direcionar para a pagina principal
+                Response.Redirect("~/index.aspx");
+            }
+            else
+            {
+                lMsg.Text = "email ou senha incorretos!!!";
+            }
+        }
+    }
+}
